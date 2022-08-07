331a746c8a4c95ea1ed703bc91c1308ea633329b
drewtn <drew.t.n@gmail.com>
2022-08-07 15:39:45 -0700

    finished step 5

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,24 +1,42 @@
-function getComputerChoice() {
-    let textArray = ['Rock', 'Paper', 'Scissors'];
+function getComputerChoice(string) {
+    let rpsArray = ['Rock', 'Paper', 'Scissors'];
 
-    let choice = textArray[Math.floor(Math.random() * textArray.length)];
-    console.log(choice);
+    return rpsArray[Math.floor(Math.random() * rpsArray.length)];
 }
 
 //define playerSelection
-let playerSelection = capitalize(prompt())
+const playerSelection = capitalize(prompt('Rock, Paper, Scissors MOTHERFUCKER LETS GO!'));
 //make playerSelection case insensitive
 function capitalize(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase()
-    console.log(capitalize(string))
+    return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+//    console.log(capitalize(string));
 }
 //define computerSelection
-let computerSelection = getComputerChoice()
-//play playerSelection with computerSelection
+const computerSelection = getComputerChoice();
+//play playerSelection with computerSelection one round
 function playRound(playerSelection, computerSelection) {
-    return 
+    if (playerSelection === 'Rock' && computerSelection === 'Scissors') {
+        return ('You Win! Rocks obliterate scissors.');
+    } else if (playerSelection === 'Rock' && computerSelection === 'Paper') {
+        return ('You Lose! Paper engulfs and smothers rock.');
+    } else if (playerSelection === 'Rock' && computerSelection === 'Rock') {
+        return ('Draw! Rocks are rocks.');
+    } else if (playerSelection === 'Paper' && computerSelection === 'Scissors') {
+        return ('You Lose! Scissors cut the fuck out of paper.');
+    } else if (playerSelection === 'Paper' && computerSelection === 'Paper') {
+        return ('Draw! Paper is paper.');
+    } else if (playerSelection === 'Paper' && computerSelection === 'Rock') {
+        return ('You Win! Paper engulfs and smothers rock.');
+    } else if (playerSelection === 'Scissors' && computerSelection === 'Scissors') {
+        return ('Draw! Scissors are scissors.');
+    } else if (playerSelection === 'Scissors' && computerSelection === 'Paper') {
+        return ('You Win! Scissors cut the fuck out of paper.');
+    } else if (playerSelection === 'Scissors' && computerSelection === 'Rock') {
+        return ('You lose! Rocks obliterate scissors.');
+    }
 }
-//should have 9 outcomes
-if playRound(Rock, Scissors) {
-    console.log("You Win! Rock beats Scissors!")
+console.log(playRound(playerSelection, computerSelection));
+//play 5 
+function game() {
+
 }
