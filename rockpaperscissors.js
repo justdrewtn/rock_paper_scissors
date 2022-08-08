1da9ccbcc928e540d3bb9985acc5885aa9fcf6d5
drewtn <drew.t.n@gmail.com>
2022-08-08 12:47:05 -0700

    Rock Paper Scissors Project Edited

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -5,7 +5,7 @@ function getComputerChoice(string) {
 }
 
 function getPlayerChoice(string) {
-    return capitalize(prompt('Rock, Paper, Scissors MOTHERFUCKER LETS GO!'))
+    return capitalize(prompt('Rock, Paper, Scissors LETS GO!'))
 }
 
 function capitalize(string) {
@@ -19,19 +19,19 @@ function playRound(playerSelection, computerSelection) {
     if (playerSelection === 'Rock' && computerSelection === 'Scissors') {
         return ('You Win! Rocks obliterate scissors.');
     } else if (playerSelection === 'Rock' && computerSelection === 'Paper') {
-        return ('You Lose! Paper engulfs and smothers the fuck out of rock.');
+        return ('You Lose! Paper engulfs and smothers rock.');
     } else if (playerSelection === 'Rock' && computerSelection === 'Rock') {
         return ('Draw! Rocks are rocks.');
     } else if (playerSelection === 'Paper' && computerSelection === 'Scissors') {
-        return ('You Lose! Scissors cut the fuck out of paper.');
+        return ('You Lose! Scissors shred paper.');
     } else if (playerSelection === 'Paper' && computerSelection === 'Paper') {
         return ('Draw! Paper is paper.');
     } else if (playerSelection === 'Paper' && computerSelection === 'Rock') {
-        return ('You Win! Paper engulfs and smothers the fuck out of rock.');
+        return ('You Win! Paper engulfs and smothers rock.');
     } else if (playerSelection === 'Scissors' && computerSelection === 'Scissors') {
         return ('Draw! Scissors are scissors.');
     } else if (playerSelection === 'Scissors' && computerSelection === 'Paper') {
-        return ('You Win! Scissors cut the fuck out of paper.');
+        return ('You Win! Scissors shred paper.');
     } else if (playerSelection === 'Scissors' && computerSelection === 'Rock') {
         return ('You Lose! Rocks obliterate scissors.');
     }
@@ -46,9 +46,9 @@ function game() {
     for (let i = 0; i < 5; i++) {
         let oneRound = playRound(getPlayerChoice(), getComputerChoice());
         console.log(oneRound);
-        if ((oneRound === 'You Win! Rocks obliterate scissors.') || (oneRound === 'You Win! Paper engulfs and smothers the fuck out of rock.') ||(oneRound === 'You Win! Scissors cut the fuck out of paper.')) {
+        if ((oneRound === 'You Win! Rocks obliterate scissors.') || (oneRound === 'You Win! Paper engulfs and smothers rock.') ||(oneRound === 'You Win! Scissors shred paper.')) {
             playerScore++;
-        } else if ((oneRound === 'You Lose! Paper engulfs and smothers the fuck out of rock.') || (oneRound === 'You Lose! Scissors cut the fuck out of paper.') ||(oneRound === 'You Lose! Rocks obliterate scissors.')) {
+        } else if ((oneRound === 'You Lose! Paper engulfs and smothers rock.') || (oneRound === 'You Lose! Scissors shred paper.') ||(oneRound === 'You Lose! Rocks obliterate scissors.')) {
             computerScore++;
         } 
     }
@@ -56,10 +56,10 @@ function game() {
     console.log('Losses - ' + computerScore);
 
     if (playerScore > computerScore) {
-        console.log('WELL FUCK, YOU WON!');
+        console.log('WELL LOOK AT THAT, YOU WON!');
     } else if (computerScore > playerScore) {
-        console.log('FUCK YOU, YOU LOST!');
+        console.log('WELL WELL WELL, YOU LOST!');
     } else if (playerScore = computerScore) {
-        console.log('GET FUCKED, WE TIED!');
+        console.log('ARGH, WE TIED!');
     }
 }
